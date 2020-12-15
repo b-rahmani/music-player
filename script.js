942a2b7ad1538c0e6b5e72c205c3ac283e557a8c
behnam <behnam.rahmani8@gmail.com>
2020-12-15 23:12:25 +0330

    add prev and next button and bug fixes

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+const image = document.querySelector("img");
+const title = document.querySelector("#title");
+const artist = document.querySelector("#artist");
+
 const music = document.querySelector("audio");
+const progressContainer = document.querySelector("#progress-container");
+const progress = document.querySelector("#progress");
+const currentTimeEl = document.querySelector("#current-time");
+const durationEl = document.querySelector("#duration");
+
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
 const playBtn = document.querySelector("#play");
+
+// music
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "electronic",
+    artist: "behnam",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "electronic",
+    artist: "ramin",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "electronic",
+    artist: "jacinto Design",
+  },
+];
 
 //check if playing
 let isPlaying = false;
@@ -29,3 +57,91 @@ function pauseSong(params) {
 playBtn.addEventListener("click", () => {
   return isPlaying ? pauseSong() : playSong();
 });
+
+//update DOM
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+
+  image.src = `img/${song.name}.jpg`;
+}
+
+//Current Song
+
+let songIndex = 0;
+
+//prev & next
+const prevSong = () => {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+const nextSong = () => {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+};
+
+//on load -Select First song
+
+loadSong(songs[songIndex]);
+
+//update progressBar &time
+
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+
+    // update progress bar Width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    //calculate display for duration
+
+    const durationMinute = Math.floor(duration / 60);
+    let durationSecconds = Math.floor(duration % 60);
+    if (durationSecconds < 10) {
+      durationSecconds = `0${durationSecconds}`;
+    }
+
+    //delay switching (NaN)
+    if (durationSecconds) {
+      durationEl.textContent = `${durationMinute}:${durationSecconds}`;
+    }
+    //calculate display for currentTime
+
+    const currentMinute = Math.floor(currentTime / 60);
+    let currentSecconds = Math.floor(currentTime % 60);
+    if (currentSecconds < 10) {
+      currentSecconds = `0${currentSecconds}`;
+    }
+    currentTimeEl.textContent = `${currentMinute}:${currentSecconds}`;
+  }
+}
+
+//set progress bar
+function setProgressBar(e) {
+  const width = e.srcElement.clientWidth;
+  let xposition = e.offsetX;
+  const { duration } = music;
+
+  music.currentTime = (xposition / width) * duration;
+}
+
+//Event Listeners
+
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
+music.addEventListener("ended", nextSong);
